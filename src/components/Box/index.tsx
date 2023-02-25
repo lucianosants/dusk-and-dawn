abdd890488128dd1973b84c345a5410e27d666a8
lucianosants <lucianola18@gmail.com>
2023-02-25 11:09:45 -0300

    feat: add box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,18 @@
+interface BoxProps {
+    as?:
+        | 'div'
+        | 'section'
+        | 'article'
+        | 'aside'
+        | 'main'
+        | 'footer'
+        | 'header'
+        | 'nav';
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export default function Box({ children, as, ...props }: BoxProps) {
+    const Tag = as || 'div';
+    return <Tag {...props}>{children}</Tag>;
+}
