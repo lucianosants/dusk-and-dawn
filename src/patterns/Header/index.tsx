1023d58ba2980cc5da9ae4cf0fe0c68ba82d21ea
lucianosants <lucianola18@gmail.com>
2023-02-26 19:48:41 -0300

    feat: add partial header component

---
+++ src/patterns/Header/index.tsx
@@ -0,0 +1,78 @@
+import { FormEvent, useState } from 'react';
+import Link from 'next/link';
+import {
+    AiOutlineSearch,
+    AiOutlineShoppingCart,
+    AiOutlineUser,
+} from 'react-icons/ai';
+
+import Box from '@/src/components/Box';
+import Text from '@/src/components/Text';
+
+export default function Header() {
+    const [count, setCount] = useState(15);
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        alert('Disponível em breve...');
+    };
+
+    return (
+        <Box className="fixed flex justify-center w-full h-16 bg-primary-600">
+            <Box
+                as="header"
+                className="flex items-center justify-between flex-1 h-full max-w-5xl px-4">
+                <Link href="/">
+                    <Text className="text-2xl italic font-black text-primary-50 hover:opacity-90">
+                        Dusk&Dawn
+                    </Text>
+                </Link>
+
+                <Box className="w-[60%]">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="flex items-center justify-between flex-1 gap-3 px-3 py-2 rounded-full bg-neutral-variant-50 focus-within:bg-primary-50 focus-within:outline hover:bg-primary-50">
+                        <label htmlFor="search" className="sr-only">
+                            Pesquise por um produto em estoque
+                        </label>
+
+                        <input
+                            className="w-full text-sm font-medium bg-transparent border-r-2 outline-none"
+                            type="text"
+                            id="search"
+                            autoComplete="off"
+                            placeholder="Pesquise por um produto..."
+                        />
+
+                        <button type="submit" title="Pesquisar produto">
+                            <Text
+                                as="span"
+                                className="text-neutral-variant-600 hover:text-neutral-variant-999">
+                                <AiOutlineSearch size={20} />
+                            </Text>
+                        </button>
+                    </form>
+                </Box>
+
+                <Box className="flex gap-6">
+                    <Link href="#">
+                        <Text
+                            data-count={count}
+                            as="span"
+                            className="cart__notification">
+                            <AiOutlineShoppingCart size={28} />
+                        </Text>
+                    </Link>
+
+                    <Link href="#">
+                        <Text
+                            as="span"
+                            className="text-neutral-variant-50 hover:text-neutral-variant-100">
+                            <AiOutlineUser size={28} />
+                        </Text>
+                    </Link>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
         './src/screens/**/*.{js,ts,jsx,tsx}',
+        './src/patterns/**/*.{js,ts,jsx,tsx}',
         './app/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
